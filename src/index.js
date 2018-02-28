f3cd4944e5ed9228e700cf6eebc69bb1cd4714e7
Sinkevich Yuliya <yuliya.sinkevich1@gmail.com>
2018-03-01 00:51:37 +0300

    solution for sixth task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+var arr=[];
+arr=str.split("");
+
+if (arr.length%2!==0) {
+return false;
+}
+
+var stack = [];
+for (var i=0; i<arr.length; i++) {
+	for (var j=0; j<bracketsConfig.length; j++) {
+		if (arr[i]==bracketsConfig[j][0]) {
+		stack.push(arr[i]);
+		} if (arr[i]==bracketsConfig[j][1]&&stack[stack.length-1]==bracketsConfig[j][0]) {
+		stack.pop();
+		}
+	}
+}
+
+if (stack.length === 0) {
+return true;
+} else return false;
+
 }
